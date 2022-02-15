d8269cfe0772a1ade79f3a40309588304ffe1a02
dttung79 <dttung@gmail.com>
2022-02-15 10:54:29 +0700

    update fraction

--- Fractions/Program.cs
+++ Fractions/Program.cs
@@ -6,8 +6,16 @@ namespace Fractions
     {
         static void Main(string[] args)
         {
-            Fraction f = new Fraction(4, 5);
-            System.Console.WriteLine(f);
+            // TODO: using GetFraction, ask user to enter 2 fractions f1, f2
+            // Print f1, f2
+            Fraction f1 = Utils.GetFraction("Enter fraction f1: ");
+            Fraction f2 = Utils.GetFraction("Enter fraction f2: ");
+
+            System.Console.WriteLine("f1 = " + f1);
+            System.Console.WriteLine("f2 = " + f2);
+
+            // TODO: Thuc hien cac phep tinh +-*/ in ra ket qua
+            // Fraction f3 = f1.Add(f2);
         }
     }
 }
